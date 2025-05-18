ad8521cf697cf4541cb6422a9c0962316c80c5f3
YASH <yashchaurasia630@gmail.com>
2025-05-18 17:49:52 +0530

    shader implementation

--- client/src/initGame.ts
+++ client/src/initGame.ts
@@ -8,20 +8,41 @@ export default async function initGame() {
     anims: {
       "walk-down-idle": 0,
       "walk-down": { from: 0, to: 3, loop: true },
-      "walk-left-down": {from: 4, to: 7, loop: true},
+      "walk-left-down": { from: 4, to: 7, loop: true },
       "walk-left-down-idle": 4,
-      "walk-left": {from: 8, to: 11, loop: true},
+      "walk-left": { from: 8, to: 11, loop: true },
       "walk-left-idle": 8,
-      "walk-left-up": {from: 12, to: 15, loop: true},
+      "walk-left-up": { from: 12, to: 15, loop: true },
       "walk-left-up-idle": 12,
-      "walk-up": {from: 16, to: 19, loop: true},
+      "walk-up": { from: 16, to: 19, loop: true },
       "walk-up-idle": 16,
-      "walk-right-up": {from: 20, to: 23, loop: true},
+      "walk-right-up": { from: 20, to: 23, loop: true },
       "walk-right-up-idle": 20,
-      "walk-right": {from: 24, to: 27, loop: true},
+      "walk-right": { from: 24, to: 27, loop: true },
       "walk-right-idle": 24,
-      "walk-right-down": {from: 28, to: 31, loop: true},
+      "walk-right-down": { from: 28, to: 31, loop: true },
       "walk-right-down-idle": 28,
     },
+  });
+  k.loadShaderURL("tiledPattern", null, "/shaders/tiledPattern.frag");
+
+  const tiledBackground = k.add([
+    k.uvquad(k.width(), k.height()),
+    k.shader("tiledPattern", () => ({
+      u_time: k.time() / 20,
+      u_color1: k.Color.fromHex("#aaea6c"),
+      u_color2: k.Color.fromHex("#ffffff"),
+      u_speed: k.vec2(1, -1),
+      u_aspect: k.width() / k.height(),
+      u_size: 5,
+    })),
+    k.pos(0, 0),
+    k.fixed(),
+  ]);
+
+  tiledBackground.onUpdate(() => {
+    tiledBackground.width = k.width();
+    tiledBackground.height = k.height();
+    tiledBackground.uniform!.u_aspect = k.width() / k.height()
   });
 }
