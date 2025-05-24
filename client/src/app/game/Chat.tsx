33a2d67eaeca4b96be524ec5537c160e71e1956f
YASH <yashchaurasia630@gmail.com>
2025-05-24 18:41:25 +0530

    live chat

--- client/src/app/game/Chat.tsx
+++ client/src/app/game/Chat.tsx
@@ -1,16 +1,50 @@
+import { FormEvent, useEffect, useMemo, useRef, useState } from "react";
+import { io } from "socket.io-client";
+
+const socket = io("http://localhost:5000");
 const Chat = () => {
+  const [messages, setMessages] = useState<{ message: string; id: string }[]>(
+    []
+  );
+  const [messageInput, setMessageInput] = useState<string>("");
+
+  const messageBox = useRef<HTMLInputElement | null>(null);
+
+  useEffect(() => {
+    socket.on("message", (data) => {
+      setMessages([...messages, data]);
+    });
+  }, [messages]);
+
+  const sendMessage = (e: FormEvent) => {
+    e.preventDefault();
+    if (messageInput) {
+      socket.emit("message", messageInput);
+      setMessageInput("");
+    }
+  };
+
+  const messageLog = useMemo(() => {
+    return messages.map((message, index) => <div key={index}>{`${message.id}: ${message.message}`}</div>);
+  }, [messages]);
+
   return (
     <div className="absolute left-0 bottom-0 ml-2 mb-2 w-[400px]">
       <div className=" bg-[#00000099] ml-1 rounded-md p-4">
-        user: hello
+        {messageLog}
       </div>
       <div className="flex items-center pl-4 gap-x-3 rounded-full bg-[#00000099] mt-3">
         <p>Chat: </p>
-        <input
-          type="text"
-          className="w-full outline-none h-[40px] pl-3 focus:border-2 rounded-full bg-[#00000099]"
-          placeholder="Press enter to chat"
-        />
+        <form className="w-full" onSubmit={sendMessage}>
+          <input
+            className="w-full outline-none h-[40px] pl-3 focus:border-2 rounded-full bg-[#00000099]"
+            type="text"
+            ref={messageBox}
+            value={messageInput}
+            onChange={(e) => setMessageInput(e.target.value)}
+            placeholder="Press enter to chat"
+          />
+        </form>
       </div>
     </div>
   );
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,44 @@
 import express from "express";
 import { createServer } from "node:http";
 import dotenv from "dotenv";
+import { Server } from "socket.io";
+import cors from "cors";
 
 dotenv.config();
+
 const app = express();
 const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    credentials: true,
+  },
+});
+
 const PORT = process.env.PORT || 8000;
+app.use(cors());
+
+app.get("/api", (req, res) => {
+  res.json({
+    message: "Hello world",
+  });
+});
+
+let id: string;
+
+io.on("connection", (socket) => {
+  id = socket.id;
+  console.log(`${socket.id} has connected`);
+
+  socket.on("disconnect", () => {
+    console.log(`${socket.id} has disconnected`);
+  });
+
+  socket.on("message", (msg) => {
+    console.log(`${id}==> ${msg}`);
+    io.emit("message", {message: msg, id: id});
+  });
+});
 
 server.listen(PORT, () => {
   console.log(`server listening on port ${PORT}`);
