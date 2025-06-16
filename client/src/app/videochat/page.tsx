4a9add465e34b849b346bcd2abcdc15008f5eec2
YASH <yashchaurasia630@gmail.com>
2025-06-16 20:51:36 +0530

    rtc client

--- client/src/app/videochat/page.tsx
+++ client/src/app/videochat/page.tsx
@@ -4,12 +4,121 @@ import React, { useEffect, useRef, useState } from "react";
 import { socket } from "../../utils";
 
 const VideoChat = () => {
+  const [joined, setJoined] = useState(false);
+  const [type, setType] = useState<"offer" | "answer">("offer");
+  const [availableCalls, setAvailableCalls] = useState([]);
+  const [userName, setUserName] = useState("");
+
   const localRef = useRef<HTMLVideoElement>(null);
   const remoteRef = useRef<HTMLVideoElement>(null);
 
   const peerConnection = useRef<RTCPeerConnection>(null);
+
+  const [offerData, setOfferData] = useState(null);
+  const [remoteStream, setRemoteStream] = useState<MediaStream | null>(null);
   const [remoteOffer, setRemoteOffer] =
     useState<RTCSessionDescriptionInit | null>(null);
+
+  const initCall = async (type: "offer" | "answer") => {
+    try {
+      // set localStream and GUM
+      const stream = await navigator.mediaDevices.getUserMedia({
+        video: true,
+        // audio: {
+        //   echoCancellation: true,
+        //   noiseSuppression: true,
+        // },
+      });
+
+      if (localRef.current) localRef.current.srcObject = stream;
+      console.log("LOCAL MEDIA ACCESS GRANTED");
+      setType(type);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const call = async (e) => {
+    initCall("offer");
+  };
+
+  const answer = async (callData) => {
+    initCall("answer");
+    setOfferData(callData);
+  };
+
+  useEffect(() => {
+    const setCalls = (data) => {
+      setAvailableCalls(data);
+      console.log(data);
+    };
+
+    setUserName(localStorage.getItem("name") || "");
+    socket.on("available_offers", setCalls);
+    socket.on("new_offer_waiting", setCalls);
+  }, [joined]);
+
+  // we have media access now setup peerconnection w/listeners
+  useEffect(() => {
+    if (!peerConnection.current && remoteStream !== null) {
+      // create peerconnection
+      const peerConfiguration = {
+        iceServers: [
+          {
+            urls: [
+              "stun:stun.google.com:19302",
+              "stun:stun1.l.google.com:5349",
+            ],
+          },
+        ],
+      };
+
+      peerConnection.current = new RTCPeerConnection(peerConfiguration);
+      setRemoteStream(new MediaStream());
+      if (remoteRef.current) remoteRef.current.srcObject = remoteStream;
+
+      peerConnection.current.addEventListener("signalingstatechange", (e) => {
+        console.log("signaling event change");
+        console.log(e);
+        console.log(peerConnection.current?.signalingState);
+      });
+
+      peerConnection.current.addEventListener("icecandidate", (e) => {
+        console.log("Foung an ICE candidate");
+        if (e.candidate) {
+          socket.emit("send_ice_candidate_to_signaling_server", {
+            iceCandidate: e.candidate,
+            iceUserName: userName,
+            didIOffer: type === "offer",
+          });
+        }
+      });
+
+      peerConnection.current.addEventListener("track", (e) => {
+        // the remote has sent us a track
+        for (const track of e.streams[0].getTracks()) {
+          remoteStream.addTrack(track);
+          console.log("This should add some audio/video to remote feed");
+        }
+      });
+    }
+  }, [remoteStream]);
+
+  // we know what kind of client this is and have a peer connection
+  useEffect(() => {
+    if (type && peerConnection.current) {
+      socket.on("answer_response", (offerObj) => {
+        console.log(offerObj);
+        const answer = offerObj.answer;
+      });
+
+      socket.on("received_ice_candidate_from_server", (iceC) => {
+        peerConnection.current?.addIceCandidate(iceC);
+        console.log(iceC);
+        console.log("Added an iceCandidate to existing page presence");
+      });
+    }
+  }, [type]);
 
   useEffect(() => {
     const handleOffer = (offers: RTCSessionDescriptionInit) => {
@@ -38,64 +147,29 @@ const VideoChat = () => {
     };
   }, []);
 
-  useEffect(() => {
-    const createOffer = async () => {
-      if (peerConnection.current) {
-        const offer = await peerConnection.current.createOffer();
-        await peerConnection.current.setLocalDescription(offer);
-        return offer;
-      }
-    };
-
-    const getUserMedia = async () => {
-      const stream = await navigator.mediaDevices.getUserMedia({
-        video: true,
-        audio: { echoCancellation: true, noiseSuppression: true },
-      });
-
-      if (localRef.current) localRef.current.srcObject = stream;
-
-      const peerConfiguration = {
-        iceServers: [
-          {
-            urls: [
-              "stun:stun.google.com:19302",
-              "stun:stun1.l.google.com:5349",
-            ],
-          },
-        ],
-      };
-      peerConnection.current = new RTCPeerConnection(peerConfiguration);
-
-      for (const track of stream.getTracks())
-        peerConnection.current.addTrack(track);
-
-      if (remoteOffer) {
-        console.log("Remote offer present. Answering...");
-
-        await peerConnection.current.setRemoteDescription(
-          new RTCSessionDescription(remoteOffer)
-        );
-
-        const answer = await peerConnection.current.createAnswer();
-        await peerConnection.current.setLocalDescription(answer);
-
-        socket.emit("rtc_answer", {
-          answer,
-        });
-      } else {
-        console.log("No remote offer. Creating and sending one.");
-        const offer = await createOffer();
-        socket.emit("rtc_offer", { offer });
-      }
-    };
-
-    getUserMedia();
-  }, [remoteOffer]);
-
   return (
     <div className="px-4 py-6">
       <h1 className="font-bold text-2xl">video chat</h1>
+      <div className="mt-4 mb-2">
+        <button
+          className="bg-blue-700 px-6 py-2 rounded-md font-semibold"
+          onClick={call}
+        >
+          Call
+        </button>
+        <div className="available-calls flex gap-x-3 items-center mt-3 text-lg font-medium">
+          <p>available calls: </p>
+          {availableCalls.map((callData, index) => (
+            <button
+              key={index}
+              className="bg-blue-700 px-6 py-2 rounded-md font-semibold"
+              onClick={() => answer(callData)}
+            >
+              {callData.offererUserName}
+            </button>
+          ))}
+        </div>
+      </div>
       <div className="flex gap-x-4 ">
         <div>
           <p>Local video</p>
--- server/src/controllers/RTCController.ts
+++ server/src/controllers/RTCController.ts
@@ -1,34 +1,42 @@
 import { io } from "../global.d.js";
 
-const offers: RTCSessionDescriptionInit[] = [];
+const offers = [
+  // offerer username
+  // offer
+  // offerer ice candidates
+  // answerer username
+  // answer
+  // answerer ice candidates
+];
+const connectedSockets = [
+  // username, socketId
+];
 
 const RTCStart = () => {
   io.on("connection", (socket) => {
-    console.log(offers);
-    socket.broadcast.emit("offers", offers[0]);
+    if (offers.length) socket.emit("available_offers", offers);
 
-    socket.on("rtc_offer", ({ offer }) => {
-      offers[0] = offer;
-    });
-
-    socket.on("call_user", (data) => {
-      socket.broadcast.emit("incoming_call", { from: socket.id, data });
-    });
-
-    socket.on("rtc_answer", ({ answer }) => {
-      io.emit("rtc_answer", {
-        senderId: socket.id,
-        answer,
-      });
-    });
-
-    socket.on("rtc_ice_candidate", ({ targetId, candidate }) => {
-      console.log(targetId, candidate);
-      io.to(targetId).emit("rtc_ice_candidate", {
-        senderId: socket.id,
-        candidate,
-      });
-    });
+    // console.log(offers);
+    // socket.broadcast.emit("offers", offers[0]);
+    // socket.on("rtc_offer", ({ offer }) => {
+    //   offers[0] = offer;
+    // });
+    // socket.on("call_user", (data) => {
+    //   socket.broadcast.emit("incoming_call", { from: socket.id, data });
+    // });
+    // socket.on("rtc_answer", ({ answer }) => {
+    //   io.emit("rtc_answer", {
+    //     senderId: socket.id,
+    //     answer,
+    //   });
+    // });
+    // socket.on("rtc_ice_candidate", ({ targetId, candidate }) => {
+    //   console.log(targetId, candidate);
+    //   io.to(targetId).emit("rtc_ice_candidate", {
+    //     senderId: socket.id,
+    //     candidate,
+    //   });
+    // });
   });
 };
 
